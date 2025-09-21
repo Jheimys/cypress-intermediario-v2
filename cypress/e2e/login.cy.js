8fe432c94be8d135d46b9743be39e5dc5d2113f6
Jheimys Bassani <jheimysbb@gmail.com>
2025-09-21 17:41:00 +0100

    1 teste

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+// describe('GitLab Community Edition', () => {
+//   beforeEach(() => {
+//     cy.visit('http://localhost/users/sign_in')
+//   })
+
+//   it('login com sucesso.', () => {
+//     cy.login('root', 'teste123456')
+//     cy.get('.blank-state-welcome-title')
+//       .should('be.visible')
+//       .and('contain', 'Welcome to GitLab')
+//   })
+// })
